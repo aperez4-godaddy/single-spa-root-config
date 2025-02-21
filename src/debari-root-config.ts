44300efcf066c345d72181fa4ee11253f29f7cdd
Abdel Pérez <abdel.perezpy@gmail.com>
2025-02-21 16:01:26 -0300

    Implement SystemJS

--- src/debari-root-config.ts
+++ src/debari-root-config.ts
@@ -5,13 +5,21 @@ import {
   constructLayoutEngine,
 } from "single-spa-layout";
 import microfrontendLayout from "./microfrontend-layout.html";
+import "systemjs";
 
-// this doesn't work bc the layout engine must be called with an html element
+System.addImportMap({
+  imports: {
+    "@debari/root-config": "//localhost:9000/debari-root-config.js",
+    "vite-mfe": "http://localhost:5173/microfrontend.js",
+  },
+});
+
 const routes = constructRoutes(microfrontendLayout);
+
 const applications = constructApplications({
   routes,
   loadApp({ name }) {
-    return import(/* webpackIgnore: true */ name);
+    return System.import(/* webpackIgnore: true */ name);
   },
 });
 const layoutEngine = constructLayoutEngine({ routes, applications });
