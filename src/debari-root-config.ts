926630c6a347ed4b1f48cf3c19f26457a4d0da08
Abdel Pérez <abdel.perezpy@gmail.com>
2025-03-21 18:53:56 -0300

    WIP: add okta sign in

--- src/debari-root-config.ts
+++ src/debari-root-config.ts
@@ -6,6 +6,9 @@ import {
 } from "single-spa-layout";
 import microfrontendLayout from "./microfrontend-layout.html";
 import "systemjs";
+import { oktaAuth } from "./okta-oauth";
+import { toRelativeUrl } from "@okta/okta-auth-js";
+import "regenerator-runtime/runtime";
 
 System.addImportMap({
   imports: {
@@ -21,8 +24,51 @@ const applications = constructApplications({
     return System.import(/* webpackIgnore: true */ name);
   },
 });
+
 const layoutEngine = constructLayoutEngine({ routes, applications });
 
 applications.forEach(registerApplication);
+
 layoutEngine.activate();
-start();
+
+async function handleOktaLoginCallback() {
+  try {
+    await oktaAuth.handleLoginRedirect();
+    const tokens = await oktaAuth.tokenManager.getTokens();
+
+    if (tokens) {
+      window.history.replaceState({}, document.title, "/");
+    } else {
+      window.history.replaceState({}, document.title, "/");
+    }
+  } catch (error) {
+    console.error("Okta login callback error:", error);
+    window.history.replaceState({}, document.title, "/");
+  }
+}
+
+// Function to initialize Okta and start single-spa conditionally
+async function initializeApp() {
+  try {
+    const isAuthenticated = await oktaAuth.isAuthenticated();
+
+    if (isAuthenticated) {
+      start();
+    } else if (window.location.pathname.startsWith("/login/callback")) {
+      await handleOktaLoginCallback();
+      start();
+    } else {
+      const originalUri = toRelativeUrl(
+        window.location.href,
+        window.location.origin
+      );
+      oktaAuth.setOriginalUri(originalUri);
+      oktaAuth.signInWithRedirect();
+      return;
+    }
+  } catch (error) {
+    console.error("Error initializing Okta:", error);
+  }
+}
+
+initializeApp();
